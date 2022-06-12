f7a4b865c794e8044b1092e06aeff8e125fd4124
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-06-12 22:07:24 +0530

    fix:fend refactor deque into a seperate component

--- frontend/src/pages/Deques.tsx
+++ frontend/src/pages/Deques.tsx
@@ -1,28 +1,8 @@
 import { Component, createSignal, lazy } from 'solid-js';
-
-const Card = lazy(() => import('../components/DigiCard'));
-const DequeMeta = lazy(() => import('../components/DequeMeta'));
-const DequeActions = lazy(() => import('../components/DequeActions'));
-
-const [answer, setAnswer] = createSignal(-1);
-const [seconds, setSeconds] = createSignal(5 * 60);
-
-function onClickAnswer(id: number) {
-	setAnswer(id);
-}
+import Deque from '../components/Deque';
 
 const Deques: Component = () => {
-	return (
-		<div class='flex justify-between'>
-			<Card answer={answer} onClickAnswer={onClickAnswer} />
-
-			<div class='mx-8 my-2 w-2/5'>
-				<DequeMeta />
-
-				<DequeActions setSeconds={setSeconds} seconds={seconds} />
-			</div>
-		</div>
-	);
+	return <Deque />;
 };
 
 export default Deques;
