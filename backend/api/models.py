f2e7a465b09ddb8fcc684e2b2ae1e31a80d6a8e0
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-06-21 06:30:35 +0530

    feat:bend add deque model(v2.1.0)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Deque(models.Model):
-    title = models.CharField(
-        name="title",  verbose_name="Title of the card",
-        blank=False, max_length=200
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='deques'
     )
     created_on = models.DateField(auto_created=True)
-    owner = models.ForeignKey(
-        'auth.User', on_delete=models.CASCADE, related_name='deques'
+    title = models.CharField(
+        verbose_name="Title of the card",
+        blank=False, max_length=200
+    )
+    category = models.CharField(
+        verbose_name="Category of the deque", max_length=200, blank=False, null=False,
+        default="N/A"
+    )
+    difficulty = models.IntegerField(
+        verbose_name="Difficulty score", blank=False, null=False, default=0
+    )
+    deque_master = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mastered_deques", blank=True, null=True
+    )
+    time = models.PositiveBigIntegerField(
+        verbose_name="Allocated Time(seconds)", default=120, blank=False, null=False
+    )
+    sidenote = models.TextField(
+        verbose_name="Sidenote for the player"
     )
 
     class Meta:
