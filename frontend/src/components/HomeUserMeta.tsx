eeba81c26ad596874f3f6ee0162d3225d2f6d6c9
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-06-22 11:11:03 +0530

    fix:fend change home user note color in light mode

--- frontend/src/components/HomeUserMeta.tsx
+++ frontend/src/components/HomeUserMeta.tsx
@@ -4,7 +4,7 @@ const HomeUserMeta: Component = () => {
 	return (
 		<div class='m-12 mt-24'>
 			<h1 class='font-prata text-5xl'>Username</h1>
-			<p class='font-noto mt-8 text-info font-bold'>
+			<p class='font-noto mt-8 text-black dark:text-info font-bold'>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo natus
 				beatae porro nostrum itaque quasi velit facilis cumque voluptates,
 				repellat magnam illum odio doloremque nulla at ut? Error, unde sequi.
